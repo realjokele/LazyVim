d8ff3b44eab2efd03b1f23ca17a32c5f7fab8ecf
realjokele <misc@jokele.de>
2026-01-11 21:04:38 +0100

    Init

---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.lsp.util.open_floating_preview = (function(orig)
+  return function(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = opts.border or "rounded"
+    return orig(contents, syntax, opts, ...)
+  end
+end)(vim.lsp.util.open_floating_preview)
---
+++ lua/plugins/lazyvim-disabled.lua
@@ -0,0 +1,4 @@
+return {
+  { "folke/noice.nvim", enabled = false },
+  { "folke/snacks.nvim", opts = { image = { enabled = false } } },
+}
